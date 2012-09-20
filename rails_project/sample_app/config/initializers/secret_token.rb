f56aaafd661365db4e124a9d8e1a01e975501435
webonise <webonise@webonise-desktop.(none)>
2012-09-20 20:30:30 +0530

    Added a Pages controller

---
+++ rails_project/sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'c18aa7358e25e1eb753517d88c013ac6faae6da98d68d8e8c3e2085ca37de784c7c5e3eb0d0a1d56577bd8897e0c1bfecf480ccfb756716744eae54b71251752'
